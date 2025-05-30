124afa4719258bd946eddf770d537be78154216d
mamorenoa <miguelangel.morenoalvarez@telefonica.com>
2025-05-30 14:12:51 +0200

    fix route

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const PORT = process.env.PORT || 3000;
 const NGROK_URL = 'https://4bda-195-235-93-3.ngrok-free.app';
 
 // Proxy GET /api/platform/getAllHguProvision
-app.get('/api/platform/getAllHguProvision', async (req, res) => {
-  try {
-    const response = await fetch(`${NGROK_URL}/api/platform/getAllHguProvision`);
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } catch (error) {
-    console.error('❌ Error al hacer proxy:', error);
-    res.status(500).json({ error: 'Error en el proxy' });
-  }
+app.get('/api/platform/hguProvision', async (req, res) => {
+	try {
+		const response = await fetch(`${NGROK_URL}/api/platform/getAllHguProvision`);
+		const data = await response.json();
+		res.status(response.status).json(data);
+	} catch (error) {
+		console.error('❌ Error al hacer proxy:', error);
+		res.status(500).json({ error: 'Error en el proxy' });
+	}
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Proxy escuchando en puerto ${PORT}`);
+	console.log(`🚀 Proxy escuchando en puerto ${PORT}`);
 });
 
